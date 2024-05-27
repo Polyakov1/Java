6be28a6bca4dd4d829062285831308e5cf79eec2
Alexey Polyakov <mrpolyakovalexey@gmail.com>
2024-05-27 07:41:14 +0500

    Java Core (семинары в записи) Урок 2. Данные и функции

--- HomeWorkTwo/src/main/Main.java
+++ HomeWorkTwo/src/main/Main.java
@@ -5,7 +5,7 @@ import AnotherClass.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array1 = {1,2,3,4,5,6,7,8,1,0};
+        int[] array1 = {1,2,3,4,5,6,7,0,1,0};
 
 
 
